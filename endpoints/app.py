074bada67b6d1e9f47218d5fe7c97c2615a32bf3
Lautaro Coo <lautarocoo@gmail.com>
2021-05-23 18:52:43 -0300

    commit of endpoints structure

---
+++ endpoints/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    pass
+
+
+@app.route('/main')
+def main():
+    pass
+
+
+@app.route('/last_response')
+def last_response():
+    pass
+
+
+@app.route('/help')
+def help():
+    pass
+
+
+@app.route('/ping')
+def health():
+    return 'Pong!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
