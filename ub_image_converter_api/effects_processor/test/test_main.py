81324bef475ea36fd1f1d0250bab1383691f2e28
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-06-04 21:24:13 -0300

    Added tests for effects_processor

--- ub_image_converter_api/effects_processor/test/test_main.py
+++ ub_image_converter_api/effects_processor/test/test_main.py
@@ -3,69 +3,39 @@ Unittests for effects_processor
 """
 from hashlib import md5
 from pathlib import Path
+from unittest.mock import patch
+
+import cv2 as cv  # type: ignore
+import numpy as np
 
 from ..main import ImgProcessor
-from .utils.hashes import get_hashes
+from .utils.hashes import get_hashes, get_effect_hash
 
 
-with open(f"{Path(__file__).parent.absolute()}/city.jpg", "rb") as f:
+with open(f"{Path(__file__).parent.absolute()}/utils/city.jpg", "rb") as f:
     image = f.read()
     test = ImgProcessor(image)
 
-HASHES = get_hashes(test)
+IMG_HASHES = get_hashes(test)
 
-def test_init():
-    """Test the constructor"""
 
-def test_blur():
-    """Test blur effect"""
-    assert HASHES["blur"] == md5(test.blur()).hexdigest()
+def test_hashes():
+    """Test the hashes dict against the processed images"""
+    IMG_HASHES.pop("noise")  # Noise effect needs a special test case
+    for effect in IMG_HASHES:
+        assert IMG_HASHES[effect] == get_effect_hash(test, effect)
 
-def test_emboss():
-    """Test emboss effect"""
-    assert HASHES["emboss"] == md5(test.emboss()).hexdigest()
-
-def test_flip():
-    """Test flip effect"""
-    assert HASHES["flip"] == md5(test.flip()).hexdigest()
-
-def test_grayscale():
-    """Test grayscale effect"""
-    assert HASHES["grayscale"] == md5(test.grayscale()).hexdigest()
-
-def test_laplacian():
-    """Test laplacian effect"""
-    assert HASHES["laplacian"] == md5(test.laplacian()).hexdigest()
-
-def test_negative():
-    """Test negative effect"""
-    assert HASHES["negative"] == md5(test.negative()).hexdigest()
 
 def test_noise():
     """Test noise effect"""
-    # assert HASHES["noise"] == md5(test.noise()).hexdigest()
-    pass
 
-def test_rotate():
-    """Test rotate effect"""
-    assert HASHES["rotate"] == md5(test.rotate()).hexdigest()
+    def mocked_noise() -> np.ndarray:
+        noise = np.zeros(test.src_image().shape, dtype=np.float64)
+        cv.randn(noise, 0, 128)
+        return cv.add(test.src_image(), np.array(1.5 * noise), dtype=cv.CV_64F)
 
-def test_scale():
-    """Test scale effect"""
-    assert HASHES["scale"] == md5(test.scale()).hexdigest()
-
-def test_sepia():
-    """Test sepia effect"""
-    assert HASHES["sepia"] == md5(test.sepia()).hexdigest()
-
-def test_sharp():
-    """Test sharp effect"""
-    assert HASHES["sharp"] == md5(test.sharp()).hexdigest()
-
-def test_sobel():
-    """Test sobel effect"""
-    assert HASHES["sobel"] == md5(test.sobel()).hexdigest()
-
-def test_src_image():
-    """Test src_image effect"""
-    assert HASHES["src_image"] == md5(test.src_image()).hexdigest()
+    noisy = mocked_noise()
+    with patch.object(test, "noise", return_value=noisy):
+        temp_img_hashes = get_hashes(test)
+        test.noise.assert_called()  # pylint: disable=no-member
+        assert temp_img_hashes["noise"] == md5(test.noise()).hexdigest()
--- ub_image_converter_api/effects_processor/test/utils/hashes.py
+++ ub_image_converter_api/effects_processor/test/utils/hashes.py
@@ -4,24 +4,25 @@ If the image for testing changes, so these hashes
 """
 from hashlib import md5
 from pathlib import Path
-from typing import Callable
 
 from ...main import ImgProcessor
 
 
-def get_effect_hash(obj: Callable, method: str) -> str:
+def get_effect_hash(obj: ImgProcessor, method: str) -> str:
     """Get the hash of the desired effect"""
     effect = getattr(obj, method, None)
     return md5(effect()).hexdigest()
 
-def get_hashes(obj: Callable) -> dict:
+
+def get_hashes(obj: ImgProcessor) -> dict:
     """Get the dict of hashes for each effect"""
     hashes = dict()
-    methods = [m for m in dir(ImgProcessor) if m.startswith('_') is False]
+    methods = [m for m in dir(ImgProcessor) if m.startswith("_") is False]
     methods.remove("effect_weight")  # This is not an effect but an attribute
     for method in methods:
         hashes[method] = get_effect_hash(obj, method)
     return hashes
+
 
 if __name__ == "__main__":
     with open(f"{Path(__file__).parent.absolute()}/city.jpg", "rb") as f:
